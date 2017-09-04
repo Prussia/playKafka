a89c06ae4a7aa9d8bf7f56f014e54fda783da95d
Prussia <prussia.hu@pwc.com>
2017-09-04 14:32:14 +0800

    Update

--- src/test/java/com/prussia/play/kafka/test/TestHelloWorld.java
+++ src/test/java/com/prussia/play/kafka/test/TestHelloWorld.java
@@ -2,6 +2,7 @@ package com.prussia.play.kafka.test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -77,7 +78,7 @@ public class TestHelloWorld {
 	private Map<String, Object> consumerProps() {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		Object group = null;
+		Object group = UUID.randomUUID().toString();
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, group);
 		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
 		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
