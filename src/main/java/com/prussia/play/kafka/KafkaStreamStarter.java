b2e3b01229bf294b5e50ff7b116eb50b57e52adf
Prussia <prussia.hu@pwc.com>
2017-09-05 17:48:39 +0800

    Update

---
+++ src/main/java/com/prussia/play/kafka/KafkaStreamStarter.java
@@ -0,0 +1,13 @@
+package com.prussia.play.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class KafkaStreamStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.info("KafkaStreamStarter Get Started");
+	}
+
+}
